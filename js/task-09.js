9c64e136db748352d836ecee2726fde7c5342b41
VSaverskyi <vovikck15@gmail.com>
2023-02-25 10:27:21 +0100

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+"use strict";
+
+const bodyEl = document.querySelector("body");
+const dataColorEl = document.querySelector(".color");
+const changeColorBtnEl = document.querySelector(".change-color");
+
+const handleChangeColor = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  dataColorEl.textContent = getRandomHexColor();
+};
+changeColorBtnEl.addEventListener("click", handleChangeColor);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
