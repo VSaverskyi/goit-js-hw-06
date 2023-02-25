db3db5d2c3d00cc58d661b35c821aba33fe00697
VSaverskyi <vovikck15@gmail.com>
2023-02-25 10:07:17 +0100

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+"use strict";
 
+const formEl = document.querySelector(".login-form");
+
+const handleCheckVerifyFormData = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  const dataObj = {};
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  dataObj.email = email.value;
+  dataObj.password = password.value;
+  console.log(dataObj);
+  formEl.reset();
+};
+
+formEl.addEventListener("submit", handleCheckVerifyFormData);
