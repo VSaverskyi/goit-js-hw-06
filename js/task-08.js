f4c4c1a8ce1f698c1dc6df81c23a44347df61181
VSaverskyi <vovikck15@gmail.com>
2023-03-16 21:29:52 +0100

    add comments

--- js/task-08.js
+++ js/task-08.js
@@ -18,3 +18,24 @@ const handleCheckVerifyFormData = (event) => {
 };
 
 formEl.addEventListener("submit", handleCheckVerifyFormData);
+// console.log(formEl.elements);
+
+// const refs = {
+//   form: document.querySelector(".login-form"),
+// };
+// refs.form.addEventListener("submit", onFormSubmit);
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   // console.dir(e.currentTarget.elements.email.value);
+//   const formData = new FormData(e.currentTarget);
+//   formData.forEach((value, name) => {
+//     if (value.length === 0) {
+//       return window.alert("Помилка! Всі поля повинні бути заповнені.");
+//     }
+
+//     formData[name] = value;
+//     // console.log("Поле:", name);
+//     // console.log("Значення:", value);
+//   });
+//   console.log(formData);
+// }
